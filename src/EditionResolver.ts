f009ca186905252c119edb2212c953074dfe99b5
Arnav G <arnavg286@gamil.com>
2021-07-16 17:46:58 -0700

    more ch

--- src/EditionResolver.ts
+++ src/EditionResolver.ts
@@ -12,6 +12,8 @@ import { isAdmin } from "./isAdmin";
 import { isAuth } from "./isAuth";
 import { kebabCase } from "./utils/strings";
 import { send } from "./sendEmail";
+import Fuse from "fuse.js";
+
 @Resolver()
 export class EditionResolver {
   @Query(() => [Edition])
@@ -76,6 +78,78 @@ export class EditionResolver {
 
       return true;
     } catch (err) {
+      return false;
+    }
+  }
+  @Query(() => Edition)
+  async getOneEdition(
+    @Arg("index", () => Int) index: number
+  ): Promise<Edition> {
+    const res = await Edition.findOne({
+      where: { num: index },
+      relations: ["articles"],
+    });
+    if (!res) {
+      throw new Error("Edition not Found");
+    }
+    return res;
+  }
+
+  @Query(() => [Edition])
+  async getSearch(@Arg("query") query: string) {
+    const ls = await Edition.find({ relations: ["articles"] });
+    let ret: any[] = [];
+    const fuse = new Fuse(ls, {
+      keys: ["name", "articles.desc", "articles.name"],
+    });
+
+    const res = fuse.search(query);
+    res.forEach((x) => {
+      ret.push(x.item);
+    });
+    return ret;
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth, isAdmin)
+  async deleteEdition(@Arg("id", () => Int) id: number) {
+    const ed = await Edition.findOne({
+      where: { id },
+      relations: ["articles"],
+    });
+    if (!ed) {
+      throw new Error("Not Found");
+    }
+    const lst: number[] = [];
+    ed.articles.forEach((x) => {
+      lst.push(x.id);
+    });
+
+    lst.forEach((y) => {
+      Article.delete({ id: y });
+    });
+
+    try {
+      await Edition.delete({ id });
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth, isAdmin)
+  async deleteArticle(@Arg("id", () => Int) id: number): Promise<boolean> {
+    const art = await Article.findOne({ where: { id } });
+    if (!art) {
+      throw new Error("Not Found");
+    }
+    try {
+      await Article.delete({ id });
+      return true;
+    } catch (error) {
+      console.log(error);
       return false;
     }
   }
